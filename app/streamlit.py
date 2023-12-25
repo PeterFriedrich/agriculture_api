eb6fb1f42311264b65cc5f11d0440d3dcfe09259
Peter <peter.friedrich.one@gmail.com>
2023-12-24 18:01:38 -0800

    added equation display to streamlit

--- app/streamlit.py
+++ app/streamlit.py
@@ -21,12 +21,14 @@ y = st.slider("Y", 0, 130, 10)
 
 # converting the inputs into a json format
 inputs = {"operation": option, "x": x, "y": y}
+operation_dict = {"Addition":"+", "Subtraction":"-", "Multiplication":"*", "Division":"/"}
 
 # when the user clicks on button it will fetch the API
 # IMPORTANT something like this saves compute
 if st.button("Calculate"):
     # json.dumps converts the inputs dictionary to json
     res = requests.post(url = "http://fastapi:8000/calculate", data = json.dumps(inputs))
+    st.subheader(f"{x} {operation_dict[option]} {y} = {res.text}")
 
     st.subheader(f"Response from API = {res.text}")
 
